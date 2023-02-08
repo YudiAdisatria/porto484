1afb39c75c62d9c03dfa37e9ceb0bcf8d905802f
YudiAdisatria <yudi.adisatria@gmail.com>
2023-02-08 10:35:49 +0700

    Update Exp

--- src/components/exp/Exp.jsx
+++ src/components/exp/Exp.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import {BsPatchCheckFill} from 'react-icons/bs'
 
 const Exp = () => {
   return (
-    <section id='exp'>Exp</section>
+    <section id='exp'>Exp
+      <h5>Skills</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__Website">
+          <h3>Website Developer</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>HTML</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>TailwindCSS</h4>
+              <small className='text-light'>Novice</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>Javascript</h4>
+              <small className='text-light'>Novice</small>
+            </article>
+          </div>
+        </div>
+
+        <div className="experience__backend">
+          <h3>Backend Developer</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>PHP</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>MySQL</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>Python</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
